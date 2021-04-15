5aecac8201e446f9c0dfa610ef63ac70184eca6f
Petlytskyi <OPetlytskyi@kiev.luxoft.com>
2021-04-15 11:29:49 +0300

    test

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,16 +1,17 @@
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from constants.urls import URLS
 
 
 class MainPage:
 
     TARIFF_BUTTON = (By.XPATH, "//a[contains(@class, 'btn') and text() = 'Точный рассчет стоимости сайта']")
+    STIM_RB_RF_BUTTON = (By.XPATH, "//div[contains(@style, 'stim')]")
 
     def __init__(self, browser):
         self.browser = browser
 
-    def load(self, needed_link):
+    def open_main_page(self, needed_link):
         self.browser.get(needed_link)
 
     def open_modal_window(self):
@@ -20,3 +21,9 @@ class MainPage:
     def to_check_if_traffic_button_text_correct(self):
         tariff_button = self.browser.find_element(*self.TARIFF_BUTTON)
         return tariff_button.is_displayed()
+
+    def click_stim_rb_rf_button(self):
+        stim_rb_rf_button = self.browser.find_element(*self.STIM_RB_RF_BUTTON)
+        # self.browser.execute_script('arguments[0].scrollIntoView(true);', stim_rb_rf_button)
+        self.browser.find_element_by_tag_name('body').send_keys(Keys.END)
+        stim_rb_rf_button.click()
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,22 +3,30 @@ from pages.modal_window import ModalWindow
 from pages.main_page import MainPage
 
 
-def test_modal_window(browser):
+# def test_tariff_modal_window(browser):
+#     main_page = MainPage(browser)
+#     modal_window = ModalWindow(browser)
+#     main_page.open_main_page(URLS["BASE_URL"])
+#     assert main_page.to_check_if_traffic_button_text_correct(), "Traffic button does not exist"
+#     main_page.open_modal_window()
+#     assert modal_window.to_check_modal_window_shows(), "Modal window does not exist"
+#     assert modal_window.to_check_current_step("1") == "Шаг: 1 из 4"
+#     assert modal_window.to_check_next_step_button_present(), "Next step button is not present"
+#     modal_window.select_the_first_option("490 BYN")
+#     modal_window.click_next_step_button()
+#     assert modal_window.to_check_current_step("2") == "Шаг: 2 из 4"
+#     modal_window.click_previous_button(), "previous button does not exist"
+#     assert modal_window.to_check_current_step("1") == "Шаг: 1 из 4"
+#     modal_window.close_modal_window()
+#     assert not modal_window.to_check_modal_window_shows()
+
+
+def test_stim_rb_rf_modal_window(browser):
     main_page = MainPage(browser)
-    modal_window = ModalWindow(browser)
-    main_page.load(URLS["BASE_URL"])
-    assert main_page.to_check_if_traffic_button_text_correct(), "Traffic does not exist"
-    main_page.open_modal_window()
-    assert modal_window.to_check_modal_window_shows(), "Modal window does not exist"
-    assert modal_window.to_check_current_step("1") == "Шаг: 1 из 4"
-    assert modal_window.to_check_next_step_button_present(), "Next step button is not present"
-    modal_window.select_the_first_option("490 BYN")
-    modal_window.click_next_step_button()
-    assert modal_window.to_check_current_step("2") == "Шаг: 2 из 4"
-    modal_window.click_previous_button(), "previous button does not exist"
-    assert modal_window.to_check_current_step("1") == "Шаг: 1 из 4"
-    modal_window.close_modal_window()
-    assert not modal_window.to_check_modal_window_shows()
+    main_page.open_main_page(URLS["BASE_URL"])
+    main_page.click_stim_rb_rf_button()
+    # while True:
+    #     pass
 
 
 
